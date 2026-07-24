5aecb88be04f3c7b105064af57283891b994c1fd
Juan <juandqt@gmail.com>
2026-07-24 11:48:36 +0200

    Initial Commit

---
+++ Kalorias/KaloriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaloriasApp.swift
+//  Kalorias
+//
+//  Created by Juan Quispe Ticona on 24/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct KaloriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
